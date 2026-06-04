949606ef7036c76aec2cebf715b896b02c69321f
champion <jphilippe.champion@gmail.com>
2026-06-04 22:36:26 +0200

    ajout/suppretion galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,11 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "Pages/home.html"),
     new Route("/galerie", "La Galerie", "Pages/galerie.html"),
+    new Route("/signin", "Se connecter", "Pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "Pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "Pages/auth/account.html"),
+    new Route("/editPassword", "Nouveau mot de passe", "Pages/auth/editPassword.html"),
+    
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
